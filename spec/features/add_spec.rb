5133744f2d2670303e6ce59229769ba520440d81
Laurence <ltaylorapp@gmail.com>
2019-05-22 11:47:38 +0100

    Added a feature test for adding a link to the bookmarks

--- spec/features/add_spec.rb
+++ spec/features/add_spec.rb
@@ -1,3 +1,9 @@
-# feature 'adding a link' do
-#   scenario 'should have '
-# end
+feature 'adding a link' do
+  scenario 'should display that page on /bookmarks' do
+    visit '/bookmarks'
+    click_button 'Add link'
+    fill_in 'add', with: 'http://kfc.com'
+    click_button 'Submit'
+    expect(page).to have_content('http://kfc.com')
+  end
+end
